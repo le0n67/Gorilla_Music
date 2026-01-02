11df0de6da23887d2ed574b35dcea90ce4cd52c0
vV_Leon <2996918437@qq.com>
2026-01-02 18:07:25 +0800

    'userinfo'

---
+++ gorillamusic-java/gorillamusic-web/src/main/java/com/gorillamusic/GorillaMusicWebApplication.java
@@ -0,0 +1,27 @@
+package com.gorillamusic;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Date：2026/1/2  10:15
+ * Description：TODO
+ *
+ * @author Leon
+ * @version 1.0
+ */
+
+@SpringBootApplication(scanBasePackages = {"com.gorillamusic"})
+@MapperScan(basePackages = {"com.gorillamusic.mappers"})
+@EnableTransactionManagement
+@EnableScheduling
+@EnableAsync
+public class GorillaMusicWebApplication {
+    public static void main(String[] args) {
+        System.out.println("MusicWeb start");
+        SpringApplication.run(GorillaMusicWebApplication.class, args);
+    }
+}
